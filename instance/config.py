5e932aebb0abc00ab6ac5f7da52b628efa482eab
Unekwu001 <unekwutheophilus@gmail.com>
2022-11-05 19:37:36 +0100

    added all

---
+++ instance/config.py
@@ -0,0 +1,5 @@
+DEBUG=True
+FLASK_ENVIRONMENT='development'
+SECRET_KEY='fgfogfif4354'
+SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://theo:otusegwa360@mysql-94712-0.cloudclusters.net:19427/bincompythontest'
+SQLALCHEMY_TRACK_NOTIFICATIONS=False
